9bf6e5b73a9d5d5d164c0463f68dc55960252402
Kong To <newlight77@gmail.com>
2021-06-19 18:40:19 +0200

    fix linting to get metrics on sonarqube

--- config/__init__.py
+++ config/__init__.py
@@ -1,23 +1,12 @@
 import os
 from dotenv import dotenv_values
 from .credentials_decrypt import decrypt_email_credential
+from shared.collections_util.dict_util import DefDictToObject
 
 ENV = os.getenv("ENV", default="local")
 
-# pylint: disable=too-few-public-methods
-class DefDictToObject():
-    env = ENV
-
-    def __init__(self, myDict):
-        for key, value in myDict.items():
-            print(f"{key} : {value}")
-            if type(value) == dict:
-                setattr(self, key, DefDictToObject(value))
-            else:
-                setattr(self, key, value)
-
-
 config = DefDictToObject({
+    'env': ENV,
     **dotenv_values("config/app.default.env"),
     **dotenv_values(f"config/app.{ENV}.env"),
     **dotenv_values(f"config/app_email_listener.{ENV}.env"),
--- tests/unit/shared/crypto_util/test_crypto_class.py
+++ tests/unit/shared/crypto_util/test_crypto_class.py
@@ -1,8 +1,21 @@
+import pytest
 import os.path
 import re
 from shared.crypto_util import crypto
 from shared.crypto_util.crypto_class import Crypto
 
+@pytest.fixture(autouse=True)
+def run_before_and_after_tests():
+    """Fixture to execute asserts before and after a test is run"""
+    # Setup
+
+    yield  # this is where the testing happens
+
+    # Teardown
+    if(os.path.isfile("config/crypto_secret.key.encrypted")):
+        os.remove("config/crypto_secret.key.encrypted")
+    if(os.path.isfile("config/crypto_secret.key.encrypted.decrypted")):
+        os.remove("config/crypto_secret.key.encrypted.decrypted")
 
 def test_encrypt_message_using_class():
     # Arrange :
@@ -26,7 +39,6 @@ def test_encrypt_file_using_class():
 
     # Assert :
     assert os.path.isfile("config/crypto_secret.key.encrypted")
-
 
 def test_decrypt_file_using_class():
     # Arrange :
