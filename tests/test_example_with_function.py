ad5528def098597f0e11c3e0831746df7d141108
Kong To <newlight77@gmail.com>
2021-06-27 18:04:52 +0200

    adding test examples

---
+++ tests/test_example_with_function.py
@@ -0,0 +1,17 @@
+def setup_module(module):
+    print('\nsetup_module()')
+
+def teardown_module(module):
+    print('teardown_module()')
+
+def setup_function(function):
+    print('\nsetup_function()')
+
+def teardown_function(function):
+    print('\nteardown_function()')
+
+def test_1():
+    print('-  test_1()')
+
+def test_2():
+    print('-  test_2()')
